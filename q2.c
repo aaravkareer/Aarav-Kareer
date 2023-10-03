8e2bb6c604edcc41f3a08ac548dd9b3e606d1c55
aaravkareer <146876983+aaravkareer@users.noreply.github.com>
2023-10-03 23:52:19 +0530

    Create q2.c

---
+++ q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    char a;
+
+    printf("Enter your character ");
+    scanf(" %c",&a);
+
+    printf("The ASCII value of the character is %d",a);
+
+    return 0;
+}
