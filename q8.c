bef70aad54813e4963177e7d3cb02d4c044a9916
aaravkareer <146876983+aaravkareer@users.noreply.github.com>
2023-10-03 23:59:49 +0530

    Update q8.c

--- q8.c
+++ q8.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main(){
+    int a;
+    float b;
+    char c;
+    double d;
+    
+    // Size of Variables
+    printf("Size of int: %ld bytes \n", sizeof(a));
+    printf("Size of float: %ld bytes \n", sizeof(b));
+    printf("Size of char: %ld bytes \n", sizeof(c));
+    printf("Size of double: %ld bytes", sizeof(d));
+    
+    return 0;
+}
