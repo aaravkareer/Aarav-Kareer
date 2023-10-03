021c21df892bb273781869b39fae140e1cf2ded0
aaravkareer <146876983+aaravkareer@users.noreply.github.com>
2023-10-04 00:02:53 +0530

    Create q12.c

---
+++ q12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+
+    printf("Enter the integer ");
+    scanf("%d",&a);
+
+    printf("The threee powers of the integer are %d %d %d",a, a*a, a*a*a);
+
+    return 0;
+}
